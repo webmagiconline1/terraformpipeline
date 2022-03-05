b29b8880b19b291488b4be2e6bd89dbe66754544
webmagiconline1 <78255428+webmagiconline1@users.noreply.github.com>
2022-03-05 16:00:49 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -16,9 +16,9 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = "${var.type}"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "${var.tag}"
   }
 }
